4673ff7eb9839e6fc522d511561c5e2d94767063
lmerienne <lucas.mrne@gmail.com>
2021-11-09 11:46:15 +0100

    tree

--- src/main/java/fr/ubx/poo/ubomb/go/decor/Tree.java
+++ src/main/java/fr/ubx/poo/ubomb/go/decor/Tree.java
@@ -4,8 +4,11 @@
 
 package fr.ubx.poo.ubomb.go.decor;
 import fr.ubx.poo.ubomb.game.Position;
+import fr.ubx.poo.ubomb.go.character.Player;
 
 public class Tree extends Decor {
+    @Override
+    public boolean isWalkable (Player player){return false;}
     public Tree(Position position) {
         super(position);
     }
